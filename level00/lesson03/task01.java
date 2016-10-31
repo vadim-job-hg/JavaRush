f66b838c523e0a4cbb4c45d650a67e4479166dd0
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2016-10-31 22:33:08 +0200

    Create task01.java

---
+++ level00/lesson03/task01.java
@@ -0,0 +1,23 @@
+package com.javarush.test.level00.lesson03.task01;
+
+/* Элли самая красивая
+Написать программу, выводящую на экран надпись «Элли самая красивая» 5 раз. Каждый раз – с новой строки.
+Пример вывода на экран:
+Элли самая красивая
+Элли самая красивая
+Элли самая красивая
+Элли самая красивая
+Элли самая красивая
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        String out = "Элли самая красивая";
+        System.out.println(out);
+        System.out.println(out);
+        System.out.println(out);
+        System.out.println(out);
+        System.out.println(out);
+    }
+}
