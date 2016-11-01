751c80c5802ebdb50328fcceb434ee103ad27a5f
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-01 20:06:16 +0200

    package com.javarush.test.level01.lesson04.task01;

---
+++ level01/lesson04/task01.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level01.lesson04.task01;
+
+/* Объявление трех переменных
+Объявите переменные name1, name2, name3 типа String.
+Сразу же в строке объявления присвойте им какие-нибудь разные значения.
+Значениями могут быть любые строки.
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        String name1 = "1", name2 = "2", name3 = "3";
+    }
+}
