fedf3159dbec57c9b7c271a457625ce14bd409bb
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-01 17:30:21 +0300

    commit

---
+++ level01/lesson03/task04.java
@@ -0,0 +1,18 @@
+package com.javarush.test.level01.lesson03.task04;
+
+/* Вывод на экран нескольких переменных
+Объявите переменную name типа String, переменные age, height типа int.
+Сразу же в строке объявления присвойте им любые разные значения.
+Выведите на экран все переменные в любом порядке. Каждую с новой строки.
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        String name="Pots";
+        int age=16, height=171;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
