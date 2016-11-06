c61f1ecd1e7ff2cb566be35149cae64b539129f5
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-06 09:44:23 +0200

    javarush

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # JavaRush
-http://javarush.ru/
+[http://javarush.ru](http://javarush.ru/user/reference/2a0b520a-f879-400d-b259-d214878bcb66)
---
+++ level02/lesson03/task03.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level02.lesson03.task03;
+
+/* Кодим на Java
+Напиши программу, которая выводит на экран надпись: «Будешь плохо кодить, придет Java и съест твою память».
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+       System.out.print("Будешь плохо кодить, придет Java и съест твою память");
+    }
+}
