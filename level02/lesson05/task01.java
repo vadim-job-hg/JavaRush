a7bdc81a919e3d549b7ad127c4761224aca3ae16
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-07 11:47:06 +0300

    commit

---
+++ level02/lesson05/task01.java
@@ -0,0 +1,29 @@
+package com.javarush.test.level02.lesson05.task01;
+
+/* 19
+Закоментарь максимальное количество строк, чтобы на экран вывелось число 19
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int x = 1;
+        int y = 0;
+
+        y = y + x;//1
+
+        x = x * 2;//2
+        y = y + x;//3
+
+        x = x * 2;//4
+        //y = y + x;//7
+
+        x = x * 2;//8
+        //y = y + x;//15
+
+        x = x * 2;//16
+        y = y + x;
+
+        System.out.println(y);
+    }
+}
