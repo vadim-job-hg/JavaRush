62b6cd9ea8636930357e7e1ef5fc24d474917bb1
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-07 17:00:08 +0300

    lesson5

---
+++ level02/lesson08/task03.java
@@ -0,0 +1,29 @@
+package com.javarush.test.level02.lesson08.task03;
+
+/* Минимум трех чисел
+Написать функцию, которая вычисляет минимум из трёх чисел.
+Подсказка:
+Нужно написать тело существующей функции min и исправить возвращаемое значение.
+*/
+public class Solution
+{
+    public static int min(int a, int b, int c)
+    {
+        if(a<b&&a<c)
+            return a;
+        else
+            if(b<a&&b<c)
+                return b;
+            else
+                return c;
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println( min(1,2,3) );
+        System.out.println( min(-1,-2,-3) );
+        System.out.println( min(3,5,3) );
+        System.out.println( min(10,5,10) );
+    }
+
+}
---
+++ level02/lesson08/task04.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level02.lesson08.task04;
+
+/* Минимум четырех чисел
+Написать функцию, которая вычисляет минимум из четырёх чисел.
+Функция min(a,b,c,d) должна использовать (вызывать) функцию min(a,b)
+Подсказка:
+Нужно написать тело обеих существующих функций min и исправить их возвращаемые значения.
+*/
+public class Solution
+{
+    public static int min(int a, int b, int c, int d)
+    {
+        if(a<b&&a<c&&a<d)
+            return a;
+        else
+            if(b<a&&b<c&&b<d)
+                return b;
+            else if(c<d)
+                return c;
+            else return d;
+    }
+
+    public static int min(int a, int b)
+    {
+        if(a<b)
+            return a;
+        else
+            return b;
+
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println( min(-20, -10) );
+        System.out.println( min(-20, -10, -30, -40) );
+        System.out.println( min(-20, -10, -30, 40) );
+    }
+}
