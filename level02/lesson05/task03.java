b1ddb2c109d65a5f436d573121d05fa7669ba782
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-07 11:51:31 +0300

    lesson5

---
+++ level02/lesson05/task03.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level02.lesson05.task03;
+
+/* У каждого животного должна быть хозяйка.
+Создайте объект типа Cat(кот), объект типа Dog (собака), объект типа Fish (рыбка) и объект типа Woman.
+Присвойте каждому животному владельца (owner).
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Fish fish = new Fish();
+        Woman woman=  new Woman();
+        dog.owner = woman;
+        cat.owner = woman;
+        fish.owner = woman;
+    }
+
+    public static class Cat
+    {
+        public Woman owner;
+    }
+
+    public static class Dog
+    {
+        public Woman owner;
+    }
+
+    public static class Fish
+    {
+        public Woman owner;
+    }
+
+    public static class Woman
+    {
+    }
+}
