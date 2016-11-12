52d8355ccc158bb1ecc689622438c69053b91f2a
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-11-12 15:52:39 +0300

    Futurama

---
+++ level03/lesson03/task01.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level03.lesson03.task01;
+
+/* Реализация метода
+Добавьте метод public static void printString(String s) который будет выводить переданную строку на экран.
+*/
+public class Solution
+{
+    //напишите тут ваш код
+
+    public static void main(String[] args) {
+        printString("Hello Amigo!");
+    }
+
+    public static  void printString(String s){
+        System.out.print(s);
+    }
+}
---
+++ level03/lesson03/task03.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level03.lesson03.task03;
+
+/* 10 процентов
+Реализуйте метод addTenPercent, который увеличивает переданное целое число на 10%.
+Исправьте ошибку в сигнатуре метода.
+Подсказка: подумайте, какие у этого метода входные и выходные данные.
+*/
+public class Solution
+{
+    public static double addTenPercent(int i) {
+        return (double)(i+(i*0.1));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addTenPercent(10));
+    }
+}
