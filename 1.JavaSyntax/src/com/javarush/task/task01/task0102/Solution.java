2fb740cbc1c132d167bc2c78fda28a460751becc
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-03-06 23:03:22 +0200

    package com.javarush.task.task01.task0102;

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0102/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0102;
+
+/* 
+Комплимент учителю
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String out = "Элли самая красивая";
+        System.out.println(out);
+        System.out.println(out);
+        System.out.println(out);
+        System.out.println(out);
+        System.out.println(out);
+    }
+}
