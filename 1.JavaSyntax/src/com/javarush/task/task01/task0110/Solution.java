9ec585b972f146928ee4523f52427c7be236f247
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-03-06 23:11:32 +0200

    package com.javarush.task.task01.task0105;

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0110/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0110;
+
+/* 
+На нашем экране — переменная
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = 'h';
+        System.out.print(a);
+    }
+}
