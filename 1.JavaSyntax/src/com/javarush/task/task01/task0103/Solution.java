078d1083223d63f06d5d6ad647cbdeb954977ee7
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-03-06 23:05:15 +0200

    package com.javarush.task.task01.task0103;

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0103;
+
+/* 
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int year = 3016;
+        System.out.print(year);
+    }
+}
