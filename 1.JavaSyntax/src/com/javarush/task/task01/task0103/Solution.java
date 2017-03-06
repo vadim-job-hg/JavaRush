8edd3cd2f862ecd372c77675f8dd09bb0de7b66e
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-03-06 23:10:06 +0200

    package com.javarush.task.task01.task0105;

--- 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -6,7 +6,6 @@ package com.javarush.task.task01.task0103;
 
 public class Solution {
     public static void main(String[] args) {
-        int year = 3016;
-        System.out.print(year);
+        //напишите тут ваш код
     }
 }
