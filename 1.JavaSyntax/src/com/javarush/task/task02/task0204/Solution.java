cdbdce9dfb73649f874ea27f7c77f06f0ec34dd6
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-07 14:14:21 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0204/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task02.task0204;
+
+/* 
+О семейных отношениях
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Man man = new Man();
+        Woman woman = new Woman();
+        man.wife = woman;
+        woman.husband = man;
+    }
+
+    public static class Man
+    {
+        public int age;
+        public int height;
+        public Woman wife;
+    }
+
+    public static class Woman
+    {
+        public int age;
+        public int height;
+        public Man husband;
+    }
+}
