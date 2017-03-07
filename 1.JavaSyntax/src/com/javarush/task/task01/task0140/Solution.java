ec7262c0f89012727815e63c037b19df8f2cc93f
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-07 12:31:05 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0140/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task01.task0140;
+
+import java.util.Scanner;
+
+/* 
+Выводим квадрат числа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print(a*a);
+    }
+}
