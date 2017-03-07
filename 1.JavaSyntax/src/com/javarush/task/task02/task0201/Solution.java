29d4a90f8ffed1128061c6631d07d1bf2bae8f80
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-07 14:06:54 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0201/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task02.task0201;
+
+/* 
+Реализуем метод print
+*/
+public class Solution {
+    public static void main(String[] args) {
+        print("Java easy to learn!");
+    }
+
+    public static void print(String s) {
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+    }
+}
