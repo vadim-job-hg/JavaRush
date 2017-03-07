e3244486ebb8f2e4d7e559d7bd469addb2bb59e6
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-07 13:46:44 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0127/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task01.task0127;
+
+/* 
+Число в квадрате
+*/
+
+public class Solution {
+    public static void main(String[] args)
+    {
+        System.out.print(sqr(5));
+    }
+
+    public static int sqr(int a)
+    {
+        return a * a;
+    }
+}
