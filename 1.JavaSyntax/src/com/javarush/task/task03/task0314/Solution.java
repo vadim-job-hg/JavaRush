3651126a606568b7315b535e9ed539c47ece125b
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-07 17:13:03 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task03.task0314;
+
+/* 
+Таблица умножения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 1; i<=10; i++) {
+            System.out.println(i + " " + 2*i + " " + 3*i + " " + 4*i + " " + 5*i + " " + 6*i + " " + 7*i + " " + 8*i + " " + 9*i + " " + 10*i);
+        }
+    }
+}
