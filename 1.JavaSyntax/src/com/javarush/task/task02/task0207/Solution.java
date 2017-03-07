5716c767c9061359452735b82c10a577e97df35a
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-07 14:25:30 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0207/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task02.task0207;
+
+/* 
+Кусочек калькулятора
+*/
+public class Solution {
+    public static void main(String[] args) {
+        int a = 1,b=2;
+        System.out.println(a+b);
+        System.out.println(a*b);
+    }
+}
