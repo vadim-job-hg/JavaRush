4572220344281dfb089effbddbbb865a6cdc8978
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-07 15:36:42 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0210/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task02.task0210;
+
+/* 
+Необъективная реальность
+*/
+public class Solution {
+    public static void main(String[] args)
+    {
+        System.out.print("Будешь плохо кодить, придет Java и съест твою память");
+    }
+}
