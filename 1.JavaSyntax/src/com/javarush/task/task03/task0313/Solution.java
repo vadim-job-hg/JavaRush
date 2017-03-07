882d73ca479ee0c4d49feb0b7efe1d9b7253ad99
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-07 16:57:09 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0313/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task03.task0313;
+
+/* 
+Мама мыла раму
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String a="Мама", b="Мыла", c="Раму";
+        System.out.println(a+b+c);
+        System.out.println(a+c+b);
+        System.out.println(b+a+c);
+        System.out.println(b+c+a);
+        System.out.println(c+a+b);
+        System.out.println(c+b+a);
+    }
+}
