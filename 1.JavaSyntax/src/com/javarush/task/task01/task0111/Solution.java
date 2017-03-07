04f238b41eca93c07c07b74e0df5531d3b3ad3ef
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-07 12:06:24 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0111/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0111;
+
+/* 
+Одной переменной недостаточно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name;
+        int age;
+        String city;
+    }
+}
