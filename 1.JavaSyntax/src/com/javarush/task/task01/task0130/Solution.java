1da61abd8b1d8899a134c89a8eb446910d35d2d3
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-07 15:06:13 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0130/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0130;
+
+/* 
+Наш первый конвертер!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(convertCelsiumToFahrenheit(40));
+    }
+
+    public static double convertCelsiumToFahrenheit(int celsium) {
+        double fahrenheit = 9 / 5. * celsium + 32.;
+        return fahrenheit;
+    }
+}
