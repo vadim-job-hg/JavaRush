6362a5b17436a4ca5c22ea1ca36573d10fc4d74f
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-07 12:35:41 +0300

    #commit

--- 1.JavaSyntax/src/com/javarush/task/task01/task0115/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0115/Solution.java
@@ -1,12 +1,11 @@
-package com.javarush.test.level01.lesson04.task03;
+package com.javarush.task.task01.task0115;
 
-/* Happy New Year
-Раскоментируйте часть кода, чтобы на экран вывелось сообщение "Happy New Year"
+/* 
+Ну это… С новым годом!
 */
-public class Solution
-{
-    public static void main(String[] args)
-    {
+
+public class Solution {
+    public static void main(String[] args) {
 //        String s = "Happy";
 //        System.out.println("New Year");
 //        System.out.println("Happy New");
