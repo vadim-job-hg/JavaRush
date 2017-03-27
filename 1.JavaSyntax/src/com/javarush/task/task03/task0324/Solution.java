60cca348439292ff6a61fa730a98553ee18d5463
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-27 18:39:59 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0324/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task03.task0324;
+
+/* 
+Меркантильные намерения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int s = 0; s < 10; s++) {
+            System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        }
+    }
+}
