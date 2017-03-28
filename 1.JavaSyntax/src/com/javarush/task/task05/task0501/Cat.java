b864503f262ebd9bf4320f2a9da012d55623d658
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-28 16:30:24 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0501/Cat.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task05.task0501;
+
+/* 
+Создание кота
+*/
+
+public class Cat {
+    //напишите тут ваш код
+    public String name;
+    public int age;
+    public int weight;
+    public int strength;
+
+    public static void main(String[] args) {
+
+    }
+}
