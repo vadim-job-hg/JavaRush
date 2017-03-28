545c4891f0d997b6ba89dd2676897f487498305d
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-28 16:46:38 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0503/Dog.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task05.task0503;
+
+
+/* 
+Геттеры и сеттеры для класса Dog
+*/
+
+public class Dog {
+    //напишите тут ваш код
+    public String name;
+    public int age;
+    public static void main(String[] args) {
+
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
