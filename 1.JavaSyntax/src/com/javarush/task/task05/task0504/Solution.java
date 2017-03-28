e9fe0b30dfeffc13b49614fe7f5229e218995391
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-28 16:53:23 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0504/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task05.task0504;
+
+
+/* 
+Трикотаж
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("1",1,1,1);
+        Cat cat2 = new Cat("2",2,2,2);
+        Cat cat3 = new Cat("3",3,4,4);
+    }
+
+    public static class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+    }
+}
