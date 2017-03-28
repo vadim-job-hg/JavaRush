7a500621842f5dddfa4eef70496ab5c6782157f1
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-28 15:48:18 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0438/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task04.task0438;
+
+/* 
+Рисуем линии
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        for(int i=1; i<=10;i++)
+           System.out.print("8");
+        for(int i=1; i<=10;i++)
+            System.out.println("8");
+
+    }
+}
