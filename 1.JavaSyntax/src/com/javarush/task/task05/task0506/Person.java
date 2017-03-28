0e8ff44cf5ab5f80a46bbe51002e787357ad1dc7
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-28 16:58:31 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0506/Person.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task05.task0506;
+
+/* 
+Человечки
+*/
+
+public class Person {
+    public String name;
+    public int age;
+    public String address;
+    public char sex;
+
+    public static void main(String[] args) {
+
+    }
+}
