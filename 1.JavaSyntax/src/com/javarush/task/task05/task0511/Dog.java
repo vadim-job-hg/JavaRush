9617503a49a5a914c8eb9da795b9884e4549eb42
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-28 17:40:53 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0511/Dog.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task05.task0511;
+
+/* 
+Создать класс Dog
+*/
+
+public class Dog {
+    private String name = null;
+    private int height = 0;
+    private String color = null;
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public void initialize(String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
