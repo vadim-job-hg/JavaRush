e047102c6888546cad7222c5858a1c34dd45c999
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-28 15:49:36 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0439/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task04.task0439;
+
+/* 
+Письмо счастья
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        for(int i=1; i<=10;i++)
+            System.out.println(name+" любит меня.");
+    }
+}
