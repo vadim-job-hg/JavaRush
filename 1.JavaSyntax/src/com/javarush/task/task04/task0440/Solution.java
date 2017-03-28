cec3ea3e01adb5ba5d40af092818c48a0b7a2cc0
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-28 15:57:22 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0440/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task04.task0440;
+
+/* 
+Достойная оплата труда
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for(int i=1; i<=100;i++)
+            System.out.println("Я никогда не буду работать за копейки. Амиго");
+    }
+}
