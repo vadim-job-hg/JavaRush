af5a088eba2f80112709100d60d0f6233222ea53
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-29 12:26:21 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0611/StringHelper.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task06.task0611;
+
+/* 
+Класс StringHelper
+*/
+
+public class StringHelper {
+    public static String multiply(String s) {
+        String result = "";
+        result = StringHelper.multiply(s, 5);
+        return result;
+    }
+
+    public static String multiply(String s, int count) {
+        String result = "";
+        for(int i = 0; i<count; i++) result += s;
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
