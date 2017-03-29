85b81144647b35f2874b11422e316f1d5212feb8
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-29 11:55:47 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0607/Cat.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task06.task0607;
+
+/* 
+Классовый счетчик
+*/
+
+public class Cat {
+    //напишите тут ваш код
+    public static int catCount=0;
+    public Cat(){
+        Cat.catCount++;
+    }
+    public static void main(String[] args) {
+
+    }
+}
