09f9dfcefa574ff3d5966a0fc38c88191f4a451d
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-29 17:42:20 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0704/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task07.task0704;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Переверни массив
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] numbers = new int[10];
+        String[] str = new String[10];
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 1; i <= 10; i++)
+        {
+            numbers[10-i] = Integer.parseInt(bis.readLine());
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(numbers[i]);
+        }
+    }
+}
+
