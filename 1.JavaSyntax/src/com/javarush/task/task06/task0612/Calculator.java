f237b426ae222726a458d4f749603066b20d6f45
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-03-29 12:31:50 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0612/Calculator.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task06.task0612;
+
+/* 
+Калькулятор
+*/
+
+
+public class Calculator {
+    public static int plus(int a, int b) {
+
+
+        return (int) a + b ;
+    }
+
+    public static int minus(int a, int b) {
+
+        return (int) a -b ;
+    }
+
+    public static int multiply(int a, int b) {
+
+        return (int) a * b;
+    }
+
+    public static double division(int a, int b) {
+
+        return (double)a / b ;
+    }
+
+    public static double percent(int a, int b) {
+
+        return (double) a * b / 100;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
