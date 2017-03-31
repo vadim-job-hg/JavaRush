869191bb0b8626d57afece7d3103f7a6a91d8315
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-04-01 00:13:37 +0300

    #commit

--- 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
@@ -1,6 +1,5 @@
 package com.javarush.task.task08.task0817;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,35 +10,12 @@ import java.util.Map;
 public class Solution {
     public static HashMap<String, String> createMap() {
         //напишите тут ваш код
-        HashMap<String, String> hm =new HashMap<>();
 
-        hm.put("Иванов", "Иван");
-        hm.put("Иванидзе", "Иван");
-        hm.put("Иваник", "Петр");
-        hm.put("Петров", "Петр");
-        hm.put("Сидоров", "Сидр");
-        hm.put("Прутков", "Кузьма");
-        hm.put("Толстой", "Алексей");
-        hm.put("Тонкий", "Лев");
-        hm.put("Пушкин", "Михаил");
-        hm.put("Кукушкин", "Сергей");
-
-        return hm;
     }
 
     public static void removeTheFirstNameDuplicates(HashMap<String, String> map) {
         //напишите тут ваш код
-        ArrayList<String> lst = new ArrayList<String>(map.values());
 
-        int count;
-        for (String str : lst) {
-            count = 0;
-            for (String str2 : lst) {
-                if (str.equals(str2))
-                    count++;
-                if (count==2) removeItemFromMapByValue (map, str);
-            }
-        }
     }
 
     public static void removeItemFromMapByValue(HashMap<String, String> map, String value) {
@@ -51,11 +27,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-//        HashMap<String, String> hm =  createMap();
-//        removeTheFirstNameDuplicates (hm);
-//
-//        for (Map.Entry <String,String> pair : hm.entrySet())
-//            System.out.println(pair.getKey()+" "+pair.getValue());
 
     }
 }
