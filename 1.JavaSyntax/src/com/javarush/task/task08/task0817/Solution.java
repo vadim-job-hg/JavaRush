cff00364b14e5cf17c70531b1b98036684177bf1
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-04-01 19:54:23 +0300

    #commit

--- 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
@@ -9,12 +9,36 @@ import java.util.Map;
 
 public class Solution {
     public static HashMap<String, String> createMap() {
-        //напишите тут ваш код
-
+        HashMap<String, String> hm = new HashMap<String, String>();
+        hm.put("Иванов", "Иван");
+        hm.put("Иванидзе", "Иван");
+        hm.put("Иваник", "Петр");
+        hm.put("Петров", "Петр");
+        hm.put("Сидоров", "Сидр");
+        hm.put("Прутков", "Кузьма");
+        hm.put("Толстой", "Алексей");
+        hm.put("Тонкий", "Лев");
+        hm.put("Пушкин", "Михаил");
+        hm.put("Кукушкин", "Сергей");
+        return hm;
     }
 
     public static void removeTheFirstNameDuplicates(HashMap<String, String> map) {
-        //напишите тут ваш код
+        HashMap<String, String> copy = new HashMap<String, String>(map);
+        for (Map.Entry pair : copy.entrySet()) {
+            int count = 0;
+            String a = (String) pair.getValue();
+            for (Map.Entry pair2 : copy.entrySet()) {
+                if (pair2.getValue().equals(a))
+                    count++;
+            }
+            if (count > 1) {
+                for (int i = 0; i < count; i++) {
+                    removeItemFromMapByValue(map, a);
+
+                }
+            }
+        }
 
     }
 
