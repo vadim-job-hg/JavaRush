33d013b4bc1c16cfe0190268f2ec914bbaa6c4a1
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-04-05 18:28:38 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0826/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task08.task0826;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Пять победителей
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+
+    public static void sort(int[] array) {
+        for (int j = 0; j < array.length; j++) { for (int i = 0; i < array.length - 1; i++) { if (array[i] < array[i + 1]) { int buffer = array[i]; array[i] = array[i + 1]; array[i + 1] = buffer; } } }
+    }
+}
