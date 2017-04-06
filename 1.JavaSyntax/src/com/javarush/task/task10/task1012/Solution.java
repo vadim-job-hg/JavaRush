6d0a1c3f802a8a81469fa20a158505f1d6527dbf
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-04-06 17:09:51 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1012/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task10.task1012;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Количество букв
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // алфавит
+        String abc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        char[] abcArray = abc.toCharArray();
+
+        ArrayList<Character> alphabet = new ArrayList<Character>();
+        for (int i = 0; i < abcArray.length; i++) {
+            alphabet.add(abcArray[i]);
+        }
+
+        // ввод строк
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            String s = reader.readLine();
+            list.add(s.toLowerCase());
+        }
+
+
+        int[] litter_count = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; for (int i = 0; i < list.size(); i++) { char[] array = list.get(i).toCharArray(); for (int j = 0; j < array.length; j++) { for (int k = 0; k < alphabet.size(); k++) { if (array[ j ] == alphabet.get(k)){ litter_count[ k ]++; } } } }
+        for (int k = 0; k < alphabet.size(); k++) { System.out.println(alphabet.get(k) + " " + litter_count[ k ]); }
+    }
+
+}
