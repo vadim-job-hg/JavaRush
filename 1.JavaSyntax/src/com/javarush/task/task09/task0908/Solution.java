d54f21e88276b523ac1dce6bf987d65267990dc4
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-04-06 12:32:41 +0300

    #commit

--- 1.JavaSyntax/src/com/javarush/task/task09/task0908/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0908/Solution.java
@@ -7,7 +7,6 @@ package com.javarush.task.task09.task0908;
 public class Solution {
     public static void main(String[] args) throws Exception {
         try {
-
             String s = null;
             String m = s.toLowerCase();
         } catch(NullPointerException e){
