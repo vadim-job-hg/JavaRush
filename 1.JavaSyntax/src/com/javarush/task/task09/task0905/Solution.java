dfb15593acad78a46b98c7ef49b534a0bf6f8446
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-04-06 11:05:07 +0300

    #commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0905/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task09.task0905;
+
+/* 
+Там, в синих глубинах стек-трейса…
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int deep = getStackTraceDeep();
+    }
+
+    public static int getStackTraceDeep() {
+        StackTraceElement[] elements = Thread.currentThread().getStackTrace();
+        System.out.println(elements.length);
+        return elements.length;
+    }
+}
+
